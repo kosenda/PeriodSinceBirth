c0085c720bbdc5f2f0dcdbb9b1bcd8fc541a0ebc
ksnd <kkkk6311@yahoo.co.jp>
2023-02-10 21:20:51 +0900

    feat: apply kts

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.4.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+    }
+}
+
+tasks.create<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,4 +13,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "PeriodSinceBirth"
-include ':app'
+include(":app")
