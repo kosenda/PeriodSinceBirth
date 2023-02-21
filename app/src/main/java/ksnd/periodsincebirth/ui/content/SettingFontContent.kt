3ace8ae076a8d85bccdf67b8390c08ed488f48b8
ksnd <kkkk6311@yahoo.co.jp>
2023-02-21 12:50:05 +0900

    chore: add preview in SettingFontContent

--- app/src/main/java/ksnd/periodsincebirth/ui/content/SettingFontContent.kt
+++ app/src/main/java/ksnd/periodsincebirth/ui/content/SettingFontContent.kt
@@ -9,7 +9,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import ksnd.periodsincebirth.PreviewStoreProvider
 import ksnd.periodsincebirth.R
 import ksnd.periodsincebirth.actions.SettingAction
 import ksnd.periodsincebirth.state.SettingState
@@ -17,6 +19,7 @@ import ksnd.periodsincebirth.state.State
 import ksnd.periodsincebirth.ui.FontType
 import ksnd.periodsincebirth.ui.parts.CustomRadioButton
 import ksnd.periodsincebirth.ui.parts.TitleCard
+import ksnd.periodsincebirth.ui.theme.PeriodSinceBirthTheme
 import org.reduxkotlin.compose.rememberDispatcher
 import org.reduxkotlin.compose.selectState
 
@@ -48,6 +51,26 @@ fun SettingFontContent() {
                 buttonText = fontType.fontName,
                 onClick = { dispatch(SettingAction.ChangeFont(fontType = fontType)) },
             )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewSettingFontContent_Light() {
+    PreviewStoreProvider {
+        PeriodSinceBirthTheme(isDarkTheme = false) {
+            SettingFontContent()
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewSettingFontContent_Dark() {
+    PreviewStoreProvider {
+        PeriodSinceBirthTheme(isDarkTheme = true) {
+            SettingFontContent()
         }
     }
 }
