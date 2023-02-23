29008ec4c4a873d0cdc73f31e6bbf489d25ff8f7
ksnd <kkkk6311@yahoo.co.jp>
2023-02-23 14:19:26 +0900

    chore: v1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "ksnd.periodsincebirth"
         minSdk = 26
         targetSdk = 33
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
