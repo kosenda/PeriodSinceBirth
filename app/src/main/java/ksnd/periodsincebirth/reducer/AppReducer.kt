6a5fcf172f6d04bdae61a5eb61a4299dfcc4f573
ksnd <kkkk6311@yahoo.co.jp>
2023-02-28 10:57:19 +0900

    chore: delete paragraph

--- app/src/main/java/ksnd/periodsincebirth/reducer/AppReducer.kt
+++ app/src/main/java/ksnd/periodsincebirth/reducer/AppReducer.kt
@@ -5,16 +5,15 @@ import ksnd.periodsincebirth.state.State
 import org.reduxkotlin.Reducer
 import org.reduxkotlin.typedReducer
 
-val appReducer: Reducer<State> =
-    typedReducer<State, AppAction> { state, action ->
-        state.run {
-            copy(
-                appState = when (action) {
-                    is AppAction.SetBirthday -> appState.copy(birthday = action.newBirthday)
-                    is AppAction.ChangeBirthday -> appState.copy(birthday = action.newBirthday)
-                    is AppAction.TransitionScreen -> appState.copy(navState = action.next)
-                    is AppAction.FetchBirthday -> appState
-                },
-            )
-        }
+val appReducer: Reducer<State> = typedReducer<State, AppAction> { state, action ->
+    state.run {
+        copy(
+            appState = when (action) {
+                is AppAction.SetBirthday -> appState.copy(birthday = action.newBirthday)
+                is AppAction.ChangeBirthday -> appState.copy(birthday = action.newBirthday)
+                is AppAction.TransitionScreen -> appState.copy(navState = action.next)
+                is AppAction.FetchBirthday -> appState
+            },
+        )
     }
+}
